308d9c49c493ed7bed91064bf0bdea3b5bf60dcb
FTWinston <ftwinston@gmail.com>
2018-11-27 14:23:10 +0000

    Saved projects now include a list of the assemblies they depend on (for endpoints or operations), and these will be loaded when a project is loaded if they aren't already.

--- Transmogrify.Data/Project.cs
+++ Transmogrify.Data/Project.cs
@@ -1,14 +1,50 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace Transmogrify.Data
 {
     public class Project
     {
-        [JsonProperty(Order = 1, ItemTypeNameHandling = TypeNameHandling.All)]
+        [JsonProperty(Order = 1, PropertyName = "Dependencies")]
+        private List<string> DependentAssemblies
+        {
+            get
+            {
+                var endPointAssemblies = EndPoints
+                    .Select(ep => ep.GetType().Assembly)
+                    .Distinct();
+
+                var operationAssemblies = Mappings
+                    .SelectMany(m => m.Operations)
+                    .Select(mo => mo.Operation.GetType().Assembly)
+                    .Distinct();
+
+                return endPointAssemblies
+                    .Union(operationAssemblies)
+                    .Select(a => a.FullName)
+                    .ToList();
+            }
+            set
+            {
+                var alreadyLoaded = new HashSet<string>(AppDomain.CurrentDomain.GetAssemblies().Select(a => a.FullName));
+
+                foreach (var assemblyName in value)
+                {
+                    if (alreadyLoaded.Contains(assemblyName))
+
+                    Assembly.Load(assemblyName);
+                }
+            }
+        }
+
+        [JsonProperty(Order = 2, ItemTypeNameHandling = TypeNameHandling.All)]
         public List<DataEndPoint> EndPoints { get; } = new List<DataEndPoint>();
 
-        [JsonProperty(Order = 2)]
+        [JsonProperty(Order = 3)]
         public List<Mapping> Mappings { get; } = new List<Mapping>();
     }
 }
