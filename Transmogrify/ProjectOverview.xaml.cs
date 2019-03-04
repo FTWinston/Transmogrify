87c8df666963f204d4bfa3a0e572e590c00b0ea6
FTWinston <ftwinston@gmail.com>
2019-03-04 14:43:40 +0000

    Rendering 3 fake "endpoints" in a canvas on the project display; they resize and move to fit the window's available space.

--- Transmogrify/ProjectOverview.xaml.cs
+++ Transmogrify/ProjectOverview.xaml.cs
@@ -25,6 +25,40 @@ namespace Transmogrify
             InitializeComponent();
         }
 
+        private void ProjectCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ResizeCanvasElements(projectCanvas.ActualWidth, projectCanvas.ActualHeight);
+        }
+
+        private void ResizeCanvasElements(double totalWidth, double totalHeight)
+        {
+            var endpointWidth = totalWidth * 0.4;
+            var endpointHeight = totalHeight * 0.4;
+
+            var aspectRatio = 100.0 / 75.0;
+
+            if (endpointWidth > endpointHeight * aspectRatio)
+                endpointWidth = endpointHeight * aspectRatio;
+            else
+                endpointHeight = endpointWidth / aspectRatio;
+
+            endpoint1.Width = endpointWidth;
+            endpoint2.Width = endpointWidth;
+            endpoint3.Width = endpointWidth;
+
+            endpoint1.Height = endpointHeight;
+            endpoint2.Height = endpointHeight;
+            endpoint3.Height = endpointHeight;
+
+            Canvas.SetLeft(endpoint1, totalWidth * 0.1);
+            Canvas.SetLeft(endpoint2, totalWidth * 0.9 - endpointWidth);
+            Canvas.SetLeft(endpoint3, totalWidth * 0.5 - endpointWidth / 2);
+
+            Canvas.SetTop(endpoint1, totalHeight * 0.1);
+            Canvas.SetTop(endpoint2, totalHeight * 0.1);
+            Canvas.SetTop(endpoint3, totalHeight * 0.9 - endpointHeight);
+        }
+
         private void ButtonExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
