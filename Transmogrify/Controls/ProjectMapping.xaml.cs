63ef6796a2fe9c2acc5b0513132bd2cefed69ef7
FTWinston <ftwinston@gmail.com>
2019-03-07 14:44:18 +0000

    Mapping should now be able to be set to curve upward or downward, and will account for their height when doing so

--- Transmogrify/Controls/ProjectMapping.xaml.cs
+++ Transmogrify/Controls/ProjectMapping.xaml.cs
@@ -20,13 +20,54 @@ namespace Transmogrify.Controls
 
         public static double BaseHeight => 30.0;
 
+        private bool? curveDownward;
+        public bool? CurveDownward
+        {
+            get { return curveDownward; }
+            set
+            {
+                curveDownward = value;
+
+                if (curveDownward.HasValue)
+                {
+                    label.VerticalContentAlignment = curveDownward.Value
+                        ? VerticalAlignment.Top
+                        : VerticalAlignment.Bottom;
+                }
+                else
+                    label.VerticalContentAlignment = VerticalAlignment.Center;
+            }
+        }
+
         protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
             base.OnRenderSizeChanged(sizeInfo);
 
+            double endY, midY;
+
+            if (curveDownward.HasValue)
+            {
+                if (curveDownward.Value)
+                {
+                    endY = BaseHeight / 2;
+                    midY = Height - endY;
+                }
+                else
+                {
+                    midY = BaseHeight / 2;
+                    endY = Height - midY;
+                }
+            }
+            else
+            {
+                endY = midY = Height / 2;
+            }
+
             line.Points.Clear();
-            line.Points.Add(new Point(0, Height / 2));
-            line.Points.Add(new Point(Width, Height / 2));
+            line.Points.Add(new Point(0, endY));
+            line.Points.Add(new Point(Width / 3, midY));
+            line.Points.Add(new Point(2 * Width / 3, midY));
+            line.Points.Add(new Point(Width, endY));
         }
     }
 }
